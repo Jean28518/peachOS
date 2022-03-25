a07f9b3d51081f4873928be29b2697f2a817d597
Jean28518 <jf_v@gmx.de>
2022-03-25 01:13:27 +0100

    remove snap store by default

--- usr/lib/peach/install.sh
+++ usr/lib/peach/install.sh
@@ -6,6 +6,7 @@ apt install vanilla-gnome-desktop -y
 apt purge gnome-mahjongg gnome-mines gnome-sudoku cheese gnome-todo gnome-documents gnome-photos deja-dup rhythmbox gnome-music gnome-weather transmission-gtk aisleriot dconf-editor usb-creator-gtk update-manager libreoffice* totem firefox ubuntu-session -y
 apt install flatpak timeshift celluloid vim htop -y
 snap remove firefox
+snap remove snap-store
 apt autoremove --purge -y
 apt dist-upgrade -y
 apt purge mpv -y
